77e004175b1990301643dccce29f3005e70f7020
rafael-men <rafaelgoncalves1822@gmail.com>
2024-08-22 16:29:17 -0300

    client initial-version

---
+++ Main/frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "./assets/pages/Login/Login";
+import "./globals.css"
+
+function App() {
+  return (
+    <Login/>
+  );
+}
+
+export default App;
---
+++ Main/frontend/src/assets/pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+const Login = () => {
+  return (
+    <div>iLogin</div>
+  )
+}
+
+export default Login
