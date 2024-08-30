e59c168e49213058f1f95f6267f7cd1162def058
rafael-men <rafaelgoncalves1822@gmail.com>
2024-08-30 19:08:39 -0300

    fixing dockerfile and connecting frontend

--- Main/frontend/src/pages/Login/Login.jsx
+++ Main/frontend/src/pages/Login/Login.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {useHistory} from 'react-router-dom'
 import './index.css'
 import logo from '../../assets/final_logo.png'
+import api from '../../services/api'
+
 const Login = () => {
+
+  const [username,setUsername] = useState()
+  const [password,setPassword] = useState()
+
+  const history = useHistory()
+
+  async function login(e) {
+    e.preventDefault()
+    const data = {
+      username,
+      password
+    }
+
+    try{
+      const response = await api.post('auth/signin',data)
+      localStorage.setItem('username',username)
+      localStorage.setItem('accessToken',response.data.token)
+
+      history.push('/book')
+    }catch (err) {
+      alert("Login Failed")
+    }
+  }
+
   return (
     <div className='login-container'>
       <section className="form">
         <img src={logo} alt='login'/>
-        <form action="">
+        <form action="" onSubmit={login}>
           <h1>Access your account</h1>
-          <input placeholder='Username' />
-          <input type="password" placeholder='Password'/>
+          <input placeholder='Username' value={username} onChange={e=>setUsername(e.target.value)} />
+          <input type="password" placeholder='Password' value={password} onChange={e=>setPassword(e.target.value)}/>
           <button className='button' type='submit'>Login</button>
         </form>
       </section>
